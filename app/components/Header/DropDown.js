19a6021253d2f217c0686cae3364b6e37f507181
Toluwalashe <asheogunleye02@gmail.com>
2023-10-27 21:20:16 +0100

    drop down hover settings

--- app/components/Header/DropDown.js
+++ app/components/Header/DropDown.js
@@ -10,12 +10,16 @@ export default function DropDown({ link, handler, pname }) {
   return (
     <li
       className={`${
-        pname === "/about"
-          ? "text-hoverColor md:border-hoverColor"
-          : "text-myBlue md:border-transparent md:hover:border-hoverColor"
+        pname === `/${link}`
+          ? "md:border-hoverColor"
+          : "md:border-transparent md:hover:border-hoverColor"
       } md:h-[15vh] grid gap-4 md:place-content-center md:border-b-[5px] transition-all md:relative`}
     >
-      <span className="flex items-center gap-2 md:hover:text-hoverColor transition-all">
+      <span
+        className={`flex items-center gap-2 md:hover:text-hoverColor transition-all ${
+          pname === `/${link}` ? "text-hoverColor" : "text-myBlue"
+        }`}
+      >
         <Link href={`/${link}`} onClick={handler}>
           {link}
         </Link>
@@ -28,10 +32,16 @@ export default function DropDown({ link, handler, pname }) {
       {show && (
         <ul className="pl-6 grid gap-4 md:absolute md:top-[100%] md:bg-[#F7FAFD] md:px-5 md:py-4 md:capitalize md:rounded-b-sm">
           <li className="md:min-w-[8vw] md:hover:text-hoverColor transition-all">
-            <Link href="/">Realty <span className="md:hidden">Department</span><span className="hidden md:inline">Dept.</span></Link>
+            <Link href="/">
+              Realty <span className="md:hidden">Department</span>
+              <span className="hidden md:inline">Dept.</span>
+            </Link>
           </li>
           <li className="md:min-w-[8vw] md:hover:text-hoverColor transition-all">
-            <Link href="/">IT <span className="md:hidden">Department</span><span className="hidden md:inline">Dept.</span></Link>
+            <Link href="/">
+              IT <span className="md:hidden">Department</span>
+              <span className="hidden md:inline">Dept.</span>
+            </Link>
           </li>
         </ul>
       )}
